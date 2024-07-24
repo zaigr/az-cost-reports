7f37170ea1b989cf5a179fd2c599d45c7606c394
Ihar Zakharau <ihar_zakharau@epam.com>
2024-07-24 12:15:47 +0200

    Add console tool to trigger mediator

---
+++ tools/AzCostTgBot.Terminal/CommandsRegistration.cs
@@ -0,0 +1,24 @@
+﻿using AzCostTgBot.Core.Commands.SendAccumulatedCostForecast;
+using AzCostTgBot.Core.Commands.SendLastBillingPeriodRgBreakdown;
+using Cocona;
+using MediatR;
+
+namespace AzCostTgBot.Terminal;
+
+public static class CommandsRegistration
+{
+    public static void AddAppCommands(this CoconaApp app)
+    {
+        app.AddSubCommand("send", cmd =>
+        {
+            cmd.AddCommand("forecast", async (IMediator mediator) =>
+            {
+                await mediator.Send(new SendAccumulatedCostForecastCommand());
+            });
+            cmd.AddCommand("rg-pie", async (IMediator mediator) =>
+            {
+                await mediator.Send(new SendLastBillingPeriodRgBreakdownCommand());
+            });
+        });
+    }
+}
